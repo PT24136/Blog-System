a2bdcdb311d7b1fa169f9a66bff4b7ed6dd4a5d4
PT24136 <pt24136@gmail.com>
2023-12-06 13:10:28 +0530

    system login page ui added

---
+++ system/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> The Prateek </title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div class= "row justify-content-center">
+         <div class="col-md-4">
+         <form action="" method="" class="mt-5">
+        <h2 class="mb-4">Login to System</h2>
+    <div class="form-floating mb-3">
+  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+  <label for="floatingInput">Email address</label>
+</div>
+<div class="form-floating">
+  <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+  <label for="floatingPassword">Password</label>
+</div>
+   <button type="submit" class="btn btn-success mt-4"> Submit</button>
+</form>
+         </div>
+    </div>
+
+
+    <script src="assets/js/jquery-3.7.1.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+</html>
