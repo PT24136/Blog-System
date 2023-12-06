a178565b60ce6b3dbea5e8dc2d8680847ee5ba31
PT24136 <pt24136@gmail.com>
2023-12-06 14:40:03 +0530

    login page made

--- system/login.php
+++ system/login.php
@@ -1,3 +1,6 @@
+<?php
+   session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +13,7 @@
 <body>
     <div class= "row justify-content-center">
          <div class="col-md-4">
-         <form action="" method="" class="mt-5">
+         <form action="login_handel.php" method="POST" class="mt-5">
         <h2 class="mb-4">Login to System</h2>
     <div class="form-floating mb-3">
   <input type="text" class="form-control" id="floatingInput"name="username" placeholder="Enter Your name">
@@ -20,6 +23,11 @@
   <input type="password" class="form-control" id="floatingPassword" name="password"placeholder="Password">
   <label for="floatingPassword">Password</label>
 </div>
+  <?php 
+    if(isset($_SESSION['error'])){
+      echo "<p class='text-danger'>".$_SESSION['error']."</p>";
+    }
+  ?>
    <button type="submit" class="btn btn-success mt-4">Login</button>
 </form>
          </div>
