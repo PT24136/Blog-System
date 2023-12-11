0c26f6007a0309dbb946b4a002df19f39d45297c
PT24136 <pt24136@gmail.com>
2023-12-11 14:39:54 +0530

    handling of login page done in this file

--- system/login_handel.php
+++ system/login_handel.php
@@ -1,30 +1,30 @@
 <?php
-     session_start();
-     include('helpers/connection.php');
-   if(isset($_POST['username'])){
-       $username = $_POST['usernmae'];
-       $password = $_POST['password'];
-     
-       if($username==null|| $password==null){
-         $_SESSION['error'] = "username and password compulsory";
-         header('Location:login.php');
-       }
-    $password = sha1($password);
-    $sql = "select * from admin where username='$username' and password='$password'";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-    $result = $stmt->fetch();
+session_start();
+include('helpers/connection.php');
 
-    if($result!=null && $result!=false){
-          $_SESSION['username']  = $username;
-          $_SESSION['user_login'] = true;
-          header('Location:index.php');
-    }
-    else{
-        $_SESSION['error'] = "username or password incorrect";
-        header('Location:login.php');
-    }
+if (isset($_POST['username'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  if ($username == null || $password == null) {
+    $_SESSION['error'] = "username and password compulsory";
+    header('Location:login.php');
+  }
+  $password = sha1($password);
+  $sql = "select * from admin where username='$username' and password='$password'";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute();
+  $result = $stmt->fetch();
+  
+  if ($result != null && $result != false) {
+    $_SESSION['username'] = $username;
+    $_SESSION['user_login'] = true;
+    header('Location:index.php');
+  } else {
+    $_SESSION['error'] = "username or password incorrect";
+    header('Location:login.php');
+  }
 
 
-   }
+}
 ?>
